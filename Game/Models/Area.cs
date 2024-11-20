6b8a47361c2e1d5b7cdc2bdb41099b3a16f0e362
Alexander Flick <54813629+AlexanderFlick@users.noreply.github.com>
2024-11-20 13:08:13 -0600

    cleanup empty space

--- Game/Models/Area.cs
+++ Game/Models/Area.cs
@@ -3,6 +3,6 @@
 public abstract record Area(string Name, List<Resource> Resources);
 
 public record BeginnerIsland(List<Resource> Resources, string Name = "Beginner Island")
-    : Area(Name,   Resources);
+    : Area(Name, Resources);
 public record Ethoria(List<Resource> Resources, string Name = "Ethoria")
     : Area(Name, Resources);
